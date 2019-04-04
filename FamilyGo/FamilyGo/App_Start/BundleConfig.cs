e33125c5d343ce66f008bafc1919a7d462e8a3b0
Zekun Di <zdii0001@student.monash.edu>
2019-04-04 21:29:41 +1100

    changed the radiobutton

--- FamilyGo/FamilyGo/App_Start/BundleConfig.cs
+++ FamilyGo/FamilyGo/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace FamilyGo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
